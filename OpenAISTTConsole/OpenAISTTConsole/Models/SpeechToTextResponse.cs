206c2e49e12476948534b066083c231ce366ea15
Sebastian Jensen <apps@tsjdev-apps.de>
2023-11-10 16:24:09 +0100

    Create SpeechToTextResponse.cs

---
+++ OpenAISTTConsole/OpenAISTTConsole/Models/SpeechToTextResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAISTTConsole.Models;
+
+/// <summary>
+///     Represents a response of the transcription
+///     request.
+/// </summary>
+internal class SpeechToTextResponse
+{
+    /// <summary>
+    ///     The transcribed text of the audio file.
+    /// </summary>
+    [JsonPropertyName("text")]
+    public string Text { get; set; } = string.Empty;    
+}
